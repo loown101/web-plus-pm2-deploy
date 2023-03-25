16294bb450abafb0e33d147864cc1b2820b17e6b
loown101 <loown101@gmail.com>
2023-03-26 01:30:42 +0300

    fix cors options

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,10 +14,22 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-app.use(cors({
-  origin: '*',
-}));
-app.options('*', cors());
+// enable cors
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  }),
+);
+app.options(
+  '*',
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
