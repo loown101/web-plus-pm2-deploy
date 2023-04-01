6be951b621079d23a290d21963de93a5a21d5741
loown101 <loown101@gmail.com>
2023-04-01 23:02:34 +0300

    fix npm

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy':
-        'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+        'node -v && cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
